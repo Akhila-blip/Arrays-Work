3a5b18e129ce2db48a384928be50570218007867
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-08 15:27:32 -0500

    Added ArrayExample3 program

---
+++ ArrayExample3.java
@@ -0,0 +1,14 @@
+class ArrayExample3
+{
+public static void main(String[]args)
+{
+int a[]=new int[7];
+a[0]=20;
+a[1]=15;
+a[2]=25;
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
