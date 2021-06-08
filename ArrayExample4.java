1c74c4b05b2d6f8c15bf089c0fe0cdf25522ddd4
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-08 16:57:35 -0500

    Added ArrayExample4 program

---
+++ ArrayExample4.java
@@ -0,0 +1,17 @@
+public class ArrayExample4
+{
+public static void main(String[]args)
+{
+Double[]a=new Double[7];
+a[0]=20d;
+a[1]=30d;
+a[2]=25d;
+System.out.println(a[0]);
+System.out.println(a.length);
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+
