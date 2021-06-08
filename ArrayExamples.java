a163ed5eb32af565c6139395ab8199eedb1c1824
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-08 17:31:28 -0500

    Added ArrayExamples program

---
+++ ArrayExamples.java
@@ -0,0 +1,16 @@
+public class ArrayExamples
+{
+public static void main(String[]args)
+{
+ Long[] l= new Long[7];
+int i=0;
+l[0]=20l;
+l[1]=15l;
+for(Long l1:l)
+{
+System.out.println(l1);
+}
+}
+}
+
+
