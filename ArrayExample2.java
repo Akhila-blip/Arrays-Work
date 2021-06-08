a56d6a6e6c0d2f3cbb2b5a6418d6e6b0819c7faa
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-08 15:17:45 -0500

    Added ArrayExample2 proggram

---
+++ ArrayExample2.java
@@ -0,0 +1,12 @@
+public class ArrayExample2
+{
+public static void main(String[]args)
+{
+int i[]=new int[7];
+ i[0]=20;
+ i[1]=15;
+ i[2]=25;
+System.out.println(i[0]);
+System.out.println(i.length);
+}
+}
